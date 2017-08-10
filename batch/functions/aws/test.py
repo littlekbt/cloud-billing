9d092a8c05c64e8084df302393c8c0dc58cf3eb1
littlekbt <kr.kubota.11@gmail.com>
2017-08-10 10:22:08 +0900

    resouceとresouce_usage_typeの対応

--- batch/functions/aws/test.py
+++ batch/functions/aws/test.py
@@ -10,8 +10,8 @@ import datetime
 s3 = boto3.client('s3')
 
 def handler(event, context):
-  #with open('tmp.csv', 'w') as data:
-  #  s3.download_fileobj("littlekbt-billing-report", "603023411334-aws-billing-csv-2017-08.csv", data)
+  with open('tmp.csv', 'w') as data:
+    s3.download_fileobj("littlekbt-billing-report", "603023411334-aws-billing-csv-2017-08.csv", data)
   pd_datas = pd.read_csv('tmp.csv')
   today = datetime.datetime.today()
   print(total(pd_datas, today))
@@ -21,20 +21,31 @@ def handler(event, context):
 
 # {aws.total.2017.08.07: 10}
 def total(pd_datas, date):
-    key = '.'.join(['aws', 'total', str(date.year), str(date.month), str(date.day), str(date.hour)])
-    return {key: round(pd_datas['TotalCost'].max(), 3)}
+  key = '.'.join(['aws', 'total', str(date.year), str(date.month), str(date.day), str(date.hour)])
+  return {key: round(pd_datas['TotalCost'].max(), 3)}
 
 # {aws.s3.2017.08.07: 8,
 # aws.lambda.2017.08.07: 7}
 def resources(pd_datas, date):
-    d = pd_datas.groupby("ProductCode")['TotalCost'].sum()
-    return d
+  d = pd_datas.groupby('ProductCode')['TotalCost'].sum()
+  m = {}
+  for index in d.index:
+    key = '.'.join(['aws', index, str(date.year), str(date.month), str(date.day), str(date.hour)])
+    m[key] = round(d[index], 3)
+
+  return m
+
 
 # {aws.lambda.Request.2017.08.07: 6,
 # aws.s3.USW1-TimedStorage-ByteHrs.2017.08.07: 2}
 def resource_usage_type(pd_datas, date):
-    d = pd_datas.groupby(["ProductCode", "UsageType"])['TotalCost'].sum()
-    return d
+  d = pd_datas.groupby(["ProductCode", "UsageType"])['TotalCost'].sum()
+  m = {}
+  for index in d.index:
+    key = '.'.join(['aws', index[0], index[1], str(date.year), str(date.month), str(date.day), str(date.hour)])
+    m[key] = round(d[index], 3)
+    
+  return m
 
 # 今度対応する
 # {aws.tag.user.name.littlekbt.2017.08.07: 6,
@@ -48,4 +59,4 @@ def resource_usage_type(pd_datas, date):
 
 # mapを渡してkeyを.で切ってvalueを登録する。
 def insert(pb_data):
-    print(pb_data)
+  print(pb_data)
